c82e158f5a4955f2790e4deb138062731a2487f4
ayushsharma-cse <ayushsharma.cse79@gmail.com>
2025-12-10 00:11:59 +0530

    Filtered Feature Added

--- src/pages/filter/index.jsx
+++ src/pages/filter/index.jsx
@@ -1,24 +1,48 @@
 import "./filter.css";
 
-const Filter = () => {
+const Filter = ({ search, setSearch, price, setPrice }) => {
+  const handleclear = () => {
+    setPrice("");
+    setSearch("");
+  };
+
   return (
     <div className="filters">
       <div className="filters_header">
         <span>Filters</span>
-        <i></i>
+        <i onClick={handleclear}></i>
       </div>
       <div className="filters_search">
         <span>Product Name / Brand</span>
-        <input type="text" placeholder="Search by name" />
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
       <div className="filters_price">
         <span>Price</span>
         <div className="high_low_filter">
-          <input type="radio" id="highlow" name="price" value={"highlow"} />
+          <input
+            type="radio"
+            id="highlow"
+            name="price"
+            value={"highlow"}
+            checked={price === "highlow"}
+            onChange={(e) => setPrice(e.target.value)}
+          />
           <label htmlFor="highlow">High to Low</label>
         </div>
         <div className="high_low_filter">
-          <input type="radio" id="lowhigh" name="price" value={"lowhigh"} />
+          <input
+            type="radio"
+            id="lowhigh"
+            name="price"
+            value={"lowhigh"}
+            checked={price === "lowhigh"}
+            onChange={(e) => setPrice(e.target.value)}
+          />
           <label htmlFor="lowhigh">Low to High</label>
         </div>
       </div>
--- src/screens/products/index.jsx
+++ src/screens/products/index.jsx
@@ -3,10 +3,39 @@ import "./products.css";
 import Filter from "../../pages/filter/index";
 import ProductCard from "../../pages/product_card/index";
 import { useSelector } from "react-redux"; // use selector is used to fetch the data from the redux store
+import { useState, useMemo } from "react";
 
 const Products = () => {
   const products = useSelector((state) => state.product.products);
-  console.log("products", products);
+
+  const [price, setPrice] = useState(""); // pass this into the filter components
+  const [search, setSearch] = useState("");
+
+  const filteredProducts = useMemo(() => {
+    if (search !== "" || price !== "") {
+      // if search exist and price also exist
+      let output = products.filter((item) => {
+        let name = false;
+        let brand = false;
+
+        name = item.name.toLowerCase().includes(search.toLowerCase());
+        brand = item.brand.toLowerCase().includes(search.toLowerCase());
+
+        return name || brand; // if either changes it will return
+      });
+
+      if (price === "highlow") {
+        output = output.sort((a, b) => b.price - a.price);
+      } else {
+        output = output.sort((a, b) => a.price - b.price);
+      }
+
+      return output;
+    } else {
+      // if empty
+      return products;
+    }
+  }, [search, price, products]);
 
   return (
     <div className="products">
@@ -16,9 +45,15 @@ const Products = () => {
         </p>
       </div>
       <div className="products_container">
-        <Filter />
+        <Filter
+          search={search}
+          setSearch={setSearch}
+          price={price}
+          setPrice={setPrice}
+        />
+
         <div className="products_content">
-          {products.map((item, index) => {
+          {filteredProducts.map((item, index) => {
             return <ProductCard data={item} key={index} />;
           })}
         </div>
